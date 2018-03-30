f4c45638416dd94359de61ad3bd24360e893eea7
lirlia <lilria5000@gmail.com>
2018-03-31 01:04:56 +0900

    book.js 謎検定1追加

--- book.js
+++ book.js
@@ -3,7 +3,7 @@ var html="";
     /* 紹介本用 */
 var result = new Array();
 
-var count = 47;
+var count = 48;
 var roop = 2;
 var checkArray = new Array();
 var number_array = new Array();
@@ -293,6 +293,12 @@ for (num of number_array) {
  	if (num == 46) {
 	  	html +="<h4>東大生ナゾトレ第四弾！<\/h4>";
 		html +='<div class="kaerebalink-box" style="text-align:left;padding-bottom:20px;font-size:small;/zoom: 1;overflow: hidden;"><div class="kaerebalink-image" style="float:left;margin:0 15px 10px 0;"><a href="http://www.amazon.co.jp/exec/obidos/ASIN/4594079075/lirlia-22/" target="_blank" ><img src="https://images-fe.ssl-images-amazon.com/images/I/51dtY--okUL._SL160_.jpg" style="border: none;" /></a></div><div class="kaerebalink-info" style="line-height:120%;/zoom: 1;overflow: hidden;"><div class="kaerebalink-name" style="margin-bottom:10px;line-height:120%"><a href="http://www.amazon.co.jp/exec/obidos/ASIN/4594079075/lirlia-22/" target="_blank" >東大ナゾトレ AnotherVisionからの挑戦状 第4巻</a><div class="kaerebalink-powered-date" style="font-size:8pt;margin-top:5px;font-family:verdana;line-height:120%">posted with <a href="http://kaereba.com" rel="nofollow" target="_blank">カエレバ</a></div></div><div class="kaerebalink-detail" style="margin-bottom:5px;">東京大学謎解き制作集団AnotherVision 扶桑社 2018-02-28    </div><div class="kaerebalink-link1" style="margin-top:10px;"><div class="shoplinkamazon" style="display:inline;margin-right:5px"><a href="http://www.amazon.co.jp/gp/search?keywords=%E6%9D%B1%E5%A4%A7%E3%83%8A%E3%82%BE%E3%83%88%E3%83%AC%20AnotherVision%E3%81%8B%E3%82%89%E3%81%AE%E6%8C%91%E6%88%A6%E7%8A%B6%20%E7%AC%AC4%E5%B7%BB&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&tag=lirlia-22" target="_blank" >Amazon</a></div><div class="shoplinkrakuten" style="display:inline;margin-right:5px"><a href="https://hb.afl.rakuten.co.jp/hgc/13bd5105.127462cc.13bd5106.ca3b9144/?pc=http%3A%2F%2Fsearch.rakuten.co.jp%2Fsearch%2Fmall%2F%25E6%259D%25B1%25E5%25A4%25A7%25E3%2583%258A%25E3%2582%25BE%25E3%2583%2588%25E3%2583%25AC%2520AnotherVision%25E3%2581%258B%25E3%2582%2589%25E3%2581%25AE%25E6%258C%2591%25E6%2588%25A6%25E7%258A%25B6%2520%25E7%25AC%25AC4%25E5%25B7%25BB%2F-%2Ff.1-p.1-s.1-sf.0-st.A-v.2%3Fx%3D0%26scid%3Daf_ich_link_urltxt%26m%3Dhttp%3A%2F%2Fm.rakuten.co.jp%2F" target="_blank" >楽天市場</a></div></div></div><div class="booklink-footer" style="clear: left"></div></div>';
+	}
+
+	//謎検定1
+ 	if (num == 47) {
+	  	html +="<h4>謎検の公式過去問題集2017<\/h4>";
+		html +='<div class="kaerebalink-box" style="text-align:left;padding-bottom:20px;font-size:small;/zoom: 1;overflow: hidden;"><div class="kaerebalink-image" style="float:left;margin:0 15px 10px 0;"><a href="https://www.amazon.co.jp/exec/obidos/ASIN/4990900472/lirlia-22/" target="_blank" ><img src="https://images-fe.ssl-images-amazon.com/images/I/415uzM%2BsmQL._SL160_.jpg" style="border: none;" /></a></div><div class="kaerebalink-info" style="line-height:120%;/zoom: 1;overflow: hidden;"><div class="kaerebalink-name" style="margin-bottom:10px;line-height:120%"><a href="https://www.amazon.co.jp/exec/obidos/ASIN/4990900472/lirlia-22/" target="_blank" >謎検 過去問題&amp;amp;練習問題集2017</a><div class="kaerebalink-powered-date" style="font-size:8pt;margin-top:5px;font-family:verdana;line-height:120%">posted with <a href="http://kaereba.com" rel="nofollow" target="_blank">カエレバ</a></div></div><div class="kaerebalink-detail" style="margin-bottom:5px;">日本謎解き能力検定協会 SCRAP出版 2017-10-13    </div><div class="kaerebalink-link1" style="margin-top:10px;"><div class="shoplinkamazon" style="display:inline;margin-right:5px"><a href="https://www.amazon.co.jp/gp/search?keywords=%E8%AC%8E%E6%A4%9C%20%E9%81%8E%E5%8E%BB%E5%95%8F%E9%A1%8C&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&tag=lirlia-22" target="_blank" >Amazon</a></div><div class="shoplinkrakuten" style="display:inline;margin-right:5px"><a href="https://hb.afl.rakuten.co.jp/hgc/13bd5105.127462cc.13bd5106.ca3b9144/?pc=http%3A%2F%2Fsearch.rakuten.co.jp%2Fsearch%2Fmall%2F%25E8%25AC%258E%25E6%25A4%259C%2520%25E9%2581%258E%25E5%258E%25BB%25E5%2595%258F%25E9%25A1%258C%2F-%2Ff.1-p.1-s.1-sf.0-st.A-v.2%3Fx%3D0%26scid%3Daf_ich_link_urltxt%26m%3Dhttp%3A%2F%2Fm.rakuten.co.jp%2F" target="_blank" >楽天市場</a></div></div></div><div class="booklink-footer" style="clear: left"></div></div>';
 	}
 }
 
