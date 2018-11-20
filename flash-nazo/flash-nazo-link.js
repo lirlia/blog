e86ce35309e7392683cc4f7d78a2bf0dec4a7849
lirlia <lilria5000@gmail.com>
2018-11-20 13:34:21 +0900

    [fix] flash-nazo-link.js 文言の変更

--- flash-nazo/flash-nazo-link.js
+++ flash-nazo/flash-nazo-link.js
@@ -1,8 +1,8 @@
 // nazotoreのカエレバリンク
 
 var html = '<p>他のシリーズはこちらから遊べます↓</p><ul class="link1">' +
-'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo">[第1弾] フラッシュ謎解きはこちら</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo2">[第2弾] フラッシュ謎解きはこちら</a></li>' +
+'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo">[第1弾] フラッシュ謎解き　に朝鮮！</a></li>' +
+'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo2">[第2弾] フラッシュ謎解き　に朝鮮！</a></li>' +
 '</ul>'
 var bookHTML = document.querySelector("div#flash-nazo-link");
 
