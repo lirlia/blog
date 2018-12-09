c17e834f7a5e3f800a6576f6d3781fb301c3fd55
lirlia <lilria5000@gmail.com>
2018-12-09 11:49:28 +0900

    [update] flash nazo 5 add

--- flash-nazo/flash-nazo-link.js
+++ flash-nazo/flash-nazo-link.js
@@ -5,6 +5,7 @@ var html = '<p>他のシリーズはこちらから遊べます↓</p><ul class=
 '<li><a href="https://www.nazomap.com/entry/201811_flash-nazo2">[第2弾] フラッシュ謎解きに挑戦！</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/201811_flash-nazo3">[第3弾] フラッシュ謎解きに挑戦！</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/201811_flash-nazo4">[第4弾] フラッシュ謎解きに挑戦！</a></li>' +
+'<li><a href="https://www.nazomap.com/entry/201812_flash-nazo5">[第5弾] フラッシュ謎解きに挑戦！</a></li>' +
 '</ul>'
 var bookHTML = document.querySelector("div#flash-nazo-link");
 
