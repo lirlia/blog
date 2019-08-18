96add74d4aa4fdb68dcab95648c2a05dbf4d1e75
lirlia <lilria5000@gmail.com>
2019-08-18 09:47:25 +0900

    [add] 現在挑戦中の問題について表示するように修正

--- flash-nazo/flash-nazo-link.js
+++ flash-nazo/flash-nazo-link.js
@@ -1,21 +1,25 @@
-// nazotoreのカエレバリンク
-
 var html = '<p>他のシリーズはこちらから遊べます↓</p><ul class="link1">' +
-'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo">[第1弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo2">[第2弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo3">[第3弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo4">[第4弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201812_flash-nazo5">[第5弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201812_flash-nazo6">[第6弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201812_flash-nazo7">[第7弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201812_flash-nazo8_1">[第8弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201901_flash-nazo9">[第9弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201901_flash-nazo10">[第10弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201905_flash-nazo11">[第11弾] フラッシュ謎解きに挑戦！</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/201907_flash-nazo12">[第12弾] フラッシュ謎解きに挑戦！</a></li>' +
+'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo">[第1弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo2">[第2弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo3">[第3弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201811_flash-nazo4">[第4弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201812_flash-nazo5">[第5弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201812_flash-nazo6">[第6弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201812_flash-nazo7">[第7弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201812_flash-nazo8_1">[第8弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201901_flash-nazo9">[第9弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201901_flash-nazo10">[第10弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201905_flash-nazo11">[第11弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/201907_flash-nazo12">[第12弾] フラッシュ謎解きに挑戦！</a><span style="display:none">　←現在挑戦中の問題</span></li>' +
 '</ul>'
-var bookHTML = document.querySelector("div#flash-nazo-link");
+var flashlinkHTML = document.querySelector("div#flash-nazo-link");
 
-if(bookHTML != null) {
-	bookHTML.innerHTML = html;
+if(flashlinkHTML != null) {
+	flashlinkHTML.innerHTML = html;
 }
+
+$("#flash-nazo-link ul a").each(function(i, elem) {
+	if ($(elem).attr("href") == location.href) {
+		$(elem).next("span").show()
+	}
+});
