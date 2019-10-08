b31c5613e97bbbc296a2cb6796ce40af9abf0490
lirlia <lilria5000@gmail.com>
2019-10-08 23:52:06 +0900

    [fix] 200nazo のdiv id修正

--- js/200-nazo-link.js
+++ js/200-nazo-link.js
@@ -1,12 +1,12 @@
 var html = '<p>他のシリーズはこちらから遊べます↓</p><ul class="link1">' +
-'<li><a href="https://www.nazomap.com/entry/20150615_nazo1">【頭の体操】ひらめき力を鍛える「謎解き200問」その1 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
-'<li><a href="https://www.nazomap.com/entry/20150615_nazo2">【頭の体操】ひらめき力を鍛える「謎解き200問」その2 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
-'<li><a href="https://www.nazomap.com/entry/20150615_nazo3">【頭の体操】ひらめき力を鍛える「謎解き200問」その3 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
-'<li><a href="https://www.nazomap.com/entry/20150615_nazo4">【頭の体操】ひらめき力を鍛える「謎解き200問」その4 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
-'<li><a href="https://www.nazomap.com/entry/20150615_nazo5">【頭の体操】ひらめき力を鍛える「謎解き200問」その5 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
-'<li><a href="https://www.nazomap.com/entry/20150615_nazo6">【頭の体操】ひらめき力を鍛える「謎解き200問」その6 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
-'<li><a href="https://www.nazomap.com/entry/20150615_nazo7">【頭の体操】ひらめき力を鍛える「謎解き200問」その7 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
-'<li><a href="https://www.nazomap.com/entry/20150615_nazo8">【頭の体操】ひらめき力を鍛える「謎解き200問」その8 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/20150615_nazo1">ひらめき力を鍛える「謎解き200問」その1 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/20150615_nazo2">ひらめき力を鍛える「謎解き200問」その2 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/20150615_nazo3">ひらめき力を鍛える「謎解き200問」その3 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/20150615_nazo4">ひらめき力を鍛える「謎解き200問」その4 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/20150615_nazo5">ひらめき力を鍛える「謎解き200問」その5 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/20150615_nazo6">ひらめき力を鍛える「謎解き200問」その6 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/20150615_nazo7">ひらめき力を鍛える「謎解き200問」その7 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
+'<li><a href="https://www.nazomap.com/entry/20150615_nazo8">ひらめき力を鍛える「謎解き200問」その8 </a><span style="display:none">　←現在挑戦中の問題</span></li>' +
 '</ul>'
 var flashlinkHTML = document.querySelector("div#two-nazo-link");
 
