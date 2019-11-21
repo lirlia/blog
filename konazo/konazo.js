700f094f35075dae72b0ade776171887ef6bac9e
lirlia <lilria5000@gmail.com>
2019-11-22 00:11:57 +0900

    [add] kisonazoのhtmlを修正

--- konazo/konazo.js
+++ konazo/konazo.js
@@ -1,5 +1,14 @@
 $(function() {
 
+
+    $(".konazo-end").append('<p>🔻他の問題への挑戦はこちら</p>')
+    $(".konazo-end").append('<ul>')
+    $(".konazo-end").append('<li><a href="https://www.nazomap.com/archive/category/%E9%9B%A3%E6%98%93%E5%BA%A6%E2%98%851">難易度★1の謎</a>に挑戦</li>')
+    $(".konazo-end").append('<li><a href="https://www.nazomap.com/archive/category/%E9%9B%A3%E6%98%93%E5%BA%A6%E2%98%852">難易度★2の謎</a>に挑戦</li>')
+    $(".konazo-end").append('<li><a href="https://www.nazomap.com/archive/category/%E9%9B%A3%E6%98%93%E5%BA%A6%E2%98%853">難易度★3の謎</a>に挑戦</li>')
+    $(".konazo-end").append('<li><a href="https://www.nazomap.com/archive/category/%E9%9B%A3%E6%98%93%E5%BA%A6%E2%98%854">難易度★4の謎</a>に挑戦</li>')
+    $(".konazo-end").append('<li><a href="https://www.nazomap.com/archive/category/%E9%9B%A3%E6%98%93%E5%BA%A6%E2%98%855">難易度★5の謎</a>に挑戦</li>')
+    $(".konazo-end").append('</ul><p> </p>')
     $(".konazo-end").append('<p>解けたら友達にも出題してあげてね！</p>')
     $(".konazo-end").append('<a target="_blank"><p class="nazo-facebook nazo-sns-box">Facebookで謎をシェア</p></a>')
     $(".konazo-end").append('<a target="_blank"><p class="nazo-twitter nazo-sns-box" >Twitterで謎をシェア</p></a>')
