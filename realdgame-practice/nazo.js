98039afe4c557dd0b220edc534fd625dfdffccef
lirlia <lilria5000@gmail.com>
2019-12-01 16:05:40 +0900

    [add] nazo.jsに謎追加して本も紹介

--- realdgame-practice/nazo.js
+++ realdgame-practice/nazo.js
@@ -5,11 +5,14 @@ var html = '<p>🔻他の練習問題にも挑戦しよう！</p>' +
 '<li><a href="https://www.nazomap.com/entry/20150605_onepice">ワンピース 謎解きキャンペーン に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/20150307_spynetabare">聡明なスパイは耳がいい に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/2014/06/12/065313">デスノートの練習問題 に挑戦</a></li>' +
+'<li><a href="https://www.nazomap.com/entry/201907_idle">ラブライブサンシャインの練習問題 に挑戦</a></li>' +
 '</ul>' +
 '<p>難易度★２</p>' +
 '<ul>' +
 '<li><a href="https://www.nazomap.com/entry/2014/06/17/224155">進撃の巨人 訓練兵最終試験 に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/20150324_babel">バベルの謎を解け に挑戦</a></li>' +
+'<li><a href="https://www.nazomap.com/entry/201910_hikanazo_nazo">地下謎2019の謎 に挑戦</a></li>' +
+'<li><a href="https://www.nazomap.com/entry/2018_chikanazo2018">地下謎2018の謎 に挑戦</a></li>' +
 '</ul>' +
 '<p>難易度★３</p>' +
 '<ul>' +
@@ -17,12 +20,14 @@ var html = '<p>🔻他の練習問題にも挑戦しよう！</p>' +
 '<li><a href="https://www.nazomap.com/entry/0207_conan3netabare">怪盗キッドからの挑戦状 に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/practice-sleepforest">悪い魔女からの挑戦状 に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/2014/06/07/010503">インペルダウンからの脱出 に挑戦</a></li>' +
-'<li><a href="https://www.nazomap.com/entry/2014/06/12/071534">オリエント急行からの脱出 に挑戦</a></li>' +
+'<li><a href="https://www.nazomap.com/entry/201907_conan_otameshi">[コナン]紺青の美術館からの脱出 に挑戦</a></li>' +
+'<li><a href="https://www.nazomap.com/entry/2014/06/12/071534">[コナン]オリエント急行からの脱出 に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/0223_toranpu">トランプ迷宮からの脱出 に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/20150330_sao">SAO ラーググリーズを救え に挑戦</a></li>' +
 '</ul>' +
 '<p>難易度★４</p>' +
 '<ul>' +
+'<li><a href="https://www.nazomap.com/entry/practice-hello-world">本屋巡り謎解き に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/0219_ginza">銀座令嬢誘拐事件 に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/1222_ansatsunetabare">暗殺教室 練習問題 に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/1202_%E5%AE%87%E5%AE%99%E6%80%AA%E7%8D%A3%E7%B7%B4%E7%BF%92%E5%95%8F%E9%A1%8C">宇宙怪獣からの脱出 に挑戦</a></li>' +
@@ -40,7 +45,11 @@ var html = '<p>🔻他の練習問題にも挑戦しよう！</p>' +
 '<ul>' +
 '<li><a href="https://www.nazomap.com/entry/konazo-kyodai">京大謎解き王決定戦 に挑戦</a></li>' +
 '<li><a href="https://www.nazomap.com/entry/201704_gantan">激ムズの正月謎 に挑戦 </a></li>' +
-'</ul>'
+'</ul>' +
+'<p><br>🔻ネットで買える質の高い問題集（オススメです）</p>' +
+'<div class="cstmreba"><div class="kaerebalink-box"><div class="kaerebalink-image"><a href="https://hb.afl.rakuten.co.jp/hgc/g00r6tx5.h67lxa60.g00r6tx5.h67lye27/kaereba_main_20191201160334461?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fyellowsubmarine%2F9784990900403%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fyellowsubmarine%2Fi%2F10141126%2F" target="_blank" ><img src="https://thumbnail.image.rakuten.co.jp/@0_mall/yellowsubmarine/cabinet/gameimages/boardgame2/imgrc0081965423.jpg?_ex=320x320" style="border: none;" /></a></div><div class="kaerebalink-info"><div class="kaerebalink-name"><a href="https://hb.afl.rakuten.co.jp/hgc/g00r6tx5.h67lxa60.g00r6tx5.h67lye27/kaereba_main_20191201160334461?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fyellowsubmarine%2F9784990900403%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fyellowsubmarine%2Fi%2F10141126%2F" target="_blank" >リアル脱出ゲームpresents究極の謎本</a><div class="kaerebalink-powered-date">posted with <a href="https://kaereba.com" rel="nofollow" target="_blank">カエレバ</a></div></div><div class="kaerebalink-detail"></div><div class="kaerebalink-link1"><div class="shoplinkrakuten"><a href="https://hb.afl.rakuten.co.jp/hgc/13bd5105.127462cc.13bd5106.ca3b9144/kaereba_main_20191201160334461?pc=https%3A%2F%2Fsearch.rakuten.co.jp%2Fsearch%2Fmall%2F%25E3%2583%25AA%25E3%2582%25A2%25E3%2583%25AB%25E8%2584%25B1%25E5%2587%25BA%25E3%2582%25B2%25E3%2583%25BC%25E3%2583%25A0presents%25E7%25A9%25B6%25E6%25A5%25B5%25E3%2581%25AE%25E8%25AC%258E%25E6%259C%25AC%2F-%2Ff.1-p.1-s.1-sf.0-st.A-v.2%3Fx%3D0%26scid%3Daf_ich_link_urltxt%26m%3Dhttp%3A%2F%2Fm.rakuten.co.jp%2F" target="_blank" >楽天市場で購入</a></div><div class="shoplinkamazon"><a href="https://www.amazon.co.jp/gp/search?keywords=%E3%83%AA%E3%82%A2%E3%83%AB%E8%84%B1%E5%87%BA%E3%82%B2%E3%83%BC%E3%83%A0presents%E7%A9%B6%E6%A5%B5%E3%81%AE%E8%AC%8E%E6%9C%AC&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&tag=lirlia-22" target="_blank" >Amazonで購入</a></div></div></div><div class="booklink-footer"></div></div></div>' +
+'<div class="cstmreba"><div class="kaerebalink-box"><div class="kaerebalink-image"><a href="https://hb.afl.rakuten.co.jp/hgc/g00q0725.h67lx82f.g00q0725.h67lya8b/kaereba_main_201912011604322409?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbook%2F16082284%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fbook%2Fi%2F19800527%2F" target="_blank" ><img src="https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/4278/9784909474278.jpg?_ex=320x320" style="border: none;" /></a></div><div class="kaerebalink-info"><div class="kaerebalink-name"><a href="https://hb.afl.rakuten.co.jp/hgc/g00q0725.h67lx82f.g00q0725.h67lya8b/kaereba_main_201912011604322409?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbook%2F16082284%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fbook%2Fi%2F19800527%2F" target="_blank" >謎検 過去問題＆練習問題集2019秋 [ SCRAP ]</a><div class="kaerebalink-powered-date">posted with <a href="https://kaereba.com" rel="nofollow" target="_blank">カエレバ</a></div></div><div class="kaerebalink-detail"></div><div class="kaerebalink-link1"><div class="shoplinkrakuten"><a href="https://hb.afl.rakuten.co.jp/hgc/13bd5105.127462cc.13bd5106.ca3b9144/kaereba_main_201912011604322409?pc=https%3A%2F%2Fsearch.rakuten.co.jp%2Fsearch%2Fmall%2F%2520%25E8%25AC%258E%25E6%25A4%259C%2520%25E9%2581%258E%25E5%258E%25BB%25E5%2595%258F%25E9%25A1%258C%25EF%25BC%2586%25E7%25B7%25B4%25E7%25BF%2592%25E5%2595%258F%25E9%25A1%258C%25E9%259B%25862019%25E7%25A7%258B%2F-%2Ff.1-p.1-s.1-sf.0-st.A-v.2%3Fx%3D0%26scid%3Daf_ich_link_urltxt%26m%3Dhttp%3A%2F%2Fm.rakuten.co.jp%2F" target="_blank" >楽天市場で購入</a></div><div class="shoplinkamazon"><a href="https://www.amazon.co.jp/gp/search?keywords=%20%E8%AC%8E%E6%A4%9C%20%E9%81%8E%E5%8E%BB%E5%95%8F%E9%A1%8C%EF%BC%86%E7%B7%B4%E7%BF%92%E5%95%8F%E9%A1%8C%E9%9B%862019%E7%A7%8B&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&tag=lirlia-22" target="_blank" >Amazonで購入</a></div></div></div><div class="booklink-footer"></div></div></div>'
+
 
 var practicelinkHTML = document.querySelector("div.realdgame-practice-link");
 
